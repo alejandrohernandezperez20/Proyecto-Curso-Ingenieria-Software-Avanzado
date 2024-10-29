635f4a204c89386517aded7fbd10f66a179e936b
alejandrohernandezperez20 <alexhp121@gmail.com>
2024-10-29 13:48:14 +0100

    Registro de profesor en proceso

--- proyecto3/registro_profesores.php
+++ proyecto3/registro_profesores.php
@@ -1,3 +1,61 @@
 <?php include 'log/header.php'?>
+<div
+class="container nt-3"
+style="border: 2px solid black;background-color:gray">
+<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+</svg>
+<h2 style="text-align:center;color:white;">REGISTRO DE PROFESORES</h2>
+<div class="mb-3">
+    <label for="" class="form-label"><b>Nombre del profesor</b></label>
+    <input
+        type="text"
+        class="form-control"
+        name=""
+        id=""
+        aria-describedby="helpId"
+        placeholder=""
+        style="width: 300px;"
+    />
+</div>
+<div class="mb-3" style="float:right;margin-top:-9%;">
+    <label for="" class="form-label"><b>DNI</b></label>
+    <input
+        type="text"
+        class="form-control"
+        name=""
+        id=""
+        aria-describedby="helpId"
+        placeholder=""
+        style="width: 300px;"
+    />
+</div>
+<div class="mb-3">
+    <label for="" class="form-label"><b>Curso</b></label>
+    <input
+        type="text"
+        class="form-control"
+        name=""
+        id=""
+        aria-describedby="helpId"
+        placeholder=""
+        style="width: 300px;"
+    />
+</div>
+<div class="mb-3" style="float:right;margin-top:-9%;">
+    <label for="" class="form-label"><b>Telefono</b></label>
+    <input
+        type="text"
+        class="form-control"
+        name=""
+        id=""
+        aria-describedby="helpId"
+        placeholder=""
+        style="width: 300px;"
+    />
+</div>
 
+
+
+</div>
 <?php include 'log/footer.php'?>
